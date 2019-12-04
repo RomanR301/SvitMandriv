0fecfcf5a107fabf4cbebd6fdd99456ab0e95e21
Roman Savchuk <roman.sav15@gmail.com>
2019-12-04 13:47:51 +0200

    transition to PC commit

--- js/renameMe.js
+++ js/renameMe.js
@@ -23,6 +23,26 @@ let front = {
       return new Flickity(document.querySelector(selector), options);
   },
 
+  toggleLang: function(){
+    document.querySelector('.lang-select-wrapper').addEventListener('click', function() {
+        this.querySelector('.lang-select').classList.toggle('open');
+    })
+    for (const option of document.querySelectorAll(".lang-option")) {
+        option.addEventListener('click', function() {
+            if (!this.classList.contains('selected')) {
+                this.parentNode.querySelector('.lang-option.selected').classList.remove('selected');
+                this.classList.add('selected');
+                this.closest('.lang-select').querySelector('.lang-select__trigger span').textContent = this.textContent;
+            }
+        })
+    }
+    window.addEventListener('click', function(e) {
+        const select = document.querySelector('.lang-select')
+        if (!select.contains(e.target)) {
+            select.classList.remove('open');
+        }
+    });
+  },
 
   toggleNav: function () {
       if (!this.hamburger.hasClass('open')) {
@@ -89,13 +109,16 @@ let front = {
           }
       });
 
-
       $(document).on('click', '.footer-navigation .menu-item-has-children > a', function (e) {
           e.preventDefault();
           if ($(window).width() + 16 < 991) {
               $(this).toggleClass('active');
           }
       });
+
+      $(document).on('click', '.lang-select-wrapper', function() {
+          self.toggleLang();
+      })
 
       $('.js-scrollLink').on('click', function () {
 
@@ -176,60 +199,7 @@ jQuery(function () {
   $(window).scroll(function (event) {
       didScroll = true;
   });
-
-//   setInterval(function () {
-//       if (didScroll) {
-//           hasScrolled();
-//           didScroll = false;
-//       }
-//   }, 250);
-
-//   function hasScrolled() {
-//       let st = $(this).scrollTop();
-
-//       // Make sure they scroll more than delta
-//       if (Math.abs(lastScrollTop - st) <= delta)
-//           return;
-
-//       // If they scrolled down and are past the navbar, add class .nav-up.
-//       // This is necessary so you never see what is "behind" the navbar.
-//       if (st > lastScrollTop && st > navbarHeight) {
-//           // Scroll Down
-//           $('.header').removeClass('--down').addClass('--up');
-//       } else {
-//           // Scroll Up
-//           if (st + $(window).height() < $(document).height()) {
-//               $('.header').removeClass('--up').addClass('--down');
-//           }
-//       }
-
-//       lastScrollTop = st;
-//   }
-
-
 });
-
-// $(function () {
-//   let detectInview = function () {
-//       let wh = $(window).height();
-//       let scrollTop = $(window).scrollTop();
-
-//       $('.detect-inview').each(function () {
-//           let el = $(this);
-//           if (el.offset().top - wh + 300 <= scrollTop && scrollTop <= el.offset().top + el.height()) {
-//               el.addClass('to-animate');
-
-//           } else {
-//               el.removeClass('to-animate');
-//           }
-//       });
-
-//   }
-//   detectInview();
-//   $(window).on('scroll', function () {
-//       detectInview();
-//   });
-// });
 
 $(function(){
   $("#header").load("header.html");
