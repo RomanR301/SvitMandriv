ac5b00d038838dfbd90652321b68eeb0aa6a922e
RomanR301 <roman.sav2nd@gmail.com>
2019-12-07 17:10:32 +0200

    banner slider test on mobile

--- js/sliders.js
+++ js/sliders.js
@@ -9,68 +9,24 @@ jQuery(function () {
   if (primaryScreenCarousel) {
       mainCarousel = front.newSlider('.primary-screen-carousel', {
           cellAlign: 'left',
-          // contain: false,
-          // pageDots: true,
-          // autoPlay: true,
-          // autoPlay: 5000,
-          // verticalCells: true,
           prevNextButtons: false
       });
   }
-
-  // let bannerCarousel = document.querySelector('.banner-carousel-main') !== null;
-  // let banner;
-  // if (bannerCarousel) {
-  //   banner = front.newSlider('.banner-carousel-main', {
-  //     cellAlign: 'left'
-  //   })
-  // }
-
-  // let bannerCarouselNav = document.querySelector('.banner-carousel-nav') !== null;
-  // let bannerNav;
-  // if (bannerCarouselNav) {
-  //   bannerNav = front.newSlider('.banner-carousel-nav', {
-  //     asNavFor: '.banner-carousel-main',
-  //     cellAlign: 'left',
-  //     accessibility: false,
-  //   })
-  // }
-
-
 });
 
-
-
-// $('.banner-carousel-main').flickity({
-//   pageDots: false,
-//   prevNextButtons: false
-// })
-// // 2nd carousel, navigation
-
-// $('.banner-carousel-nav').flickity({
-//   asNavFor: '.banner-carousel-main',
-//   // contain: true,
-//   pageDots: false,
-//   prevNextButtons: false
-
-// });
-
-// external js: flickity.pkgd.js
-
-// external js: flickity.pkgd.js
 
 var $carousel = $('.banner-carousel-main').flickity({
   prevNextButtons: false,
   wrapAround: true,
   pageDots: false
 });
+
 // Flickity instance
 var flkty = $carousel.data('flickity');
 // elements
 var $cellButtonGroup = $('.banner-carousel-nav');
 var $cellButtons = $cellButtonGroup.find('.button');
 
-// update selected cellButtons
 $carousel.on( 'select.flickity', function() {
   $cellButtons.filter('.is-selected')
     .removeClass('is-selected');
@@ -78,16 +34,16 @@ $carousel.on( 'select.flickity', function() {
     .addClass('is-selected');
 });
 
-// select cell on button click
+
 $cellButtonGroup.on( 'click', '.button', function() {
   var index = $(this).index();
   $carousel.flickity( 'select', index );
 });
-// previous
+
 $('.banner-previous').on( 'click', function() {
   $carousel.flickity('previous');
 });
-// next
+
 $('.banner-next').on( 'click', function() {
   $carousel.flickity('next');
 });
