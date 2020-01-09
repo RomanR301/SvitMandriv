fc73764d93515cfee14c0d882fb1a20f441ac060
Roman Savchuk <roman.sav15@gmail.com>
2020-01-09 16:35:40 +0200

    test

--- js/svitMandriv.js
+++ js/svitMandriv.js
@@ -81,9 +81,9 @@ let front = {
       $(document).on('click', '.hamburger-wrap', function () {
           self.toggleNav();
       });
-    //   $(document).on('click', '.nav-btn', function(){
-    //     $('.header-mobile').scrollTop(0);
-    //   });
+      $(document).on('click', '.nav-btn', function(){
+        $('.header-mobile').scrollTop(0);
+      });
       $(document).on('click', '.open-treatment', function(){
         $('html, body').animate({
             scrollTop: $(".content").offset().top
